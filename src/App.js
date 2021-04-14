0c8197abe02c99cec7d962cde5c86bdf42a5bef7
Chaitanya Patel <patelchaitanya514@gmail.com>
2021-04-14 15:06:00 -0400

    temp undefined error solved

--- src/App.js
+++ src/App.js
@@ -6,41 +6,37 @@ import Search from "./Search"
 const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
 function App() {
 
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState([]);
 
 
   useEffect(() => {
-    fetch(weatherApi)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        setWeather(jsonResponse);
+    const initialFetching = async () => {
+      await fetch(weatherApi)
+        .then(response => response.json())
+        .then(jsonResponse => {
+          setWeather(jsonResponse);
 
-      });
+        });
+    }
+    initialFetching();
+
   }, []);
 
-  const search = city_name => {
+  const search = async (city_name) => {
 
-
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`)
+    await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`)
       .then(response => response.json())
       .then(jsonResponse => {
         setWeather(jsonResponse);
-
       });
-
-
   }
   return (
     <div className="App">
       <Search search={search} />
 
-      {
-        <div>
-          <h2>
-            <Weather weather={weather} />
-          </h2>
-        </div>
-        }
+      { (typeof weather.main !== 'undefined') ? <h2><Weather weather={weather} /></h2> : <div><h2>Error Caught</h2></div>}
+
+
 
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 
 const Search = (props) => {
   const [city, setCity] = useState("");
-  
+
   const handleSearchInputChanges = (e) => {
     setCity(e.target.value);
   }
@@ -19,18 +31,24 @@ const Search = (props) => {
   }
 
   return (
-      <form className="search">
-        <input
-          value={city}
-          onChange={handleSearchInputChanges}
-          type="text"
-          placeholder="Type Canada's city name here..."
-        />
-        <input type="submit"
-        onClick={callSearchFunction}  
+    <form className="search">
+
+
+      <TextField
+        error
+        value={city}
+        id="filled-error-helper-text"
+        helperText="Incorrect entry."
+        variant="filled"
+        label=  "Search City Here"
+        onChange={handleSearchInputChanges}
+
+      />
+      <input type="submit"
+        onClick={callSearchFunction}
         value="SEARCH" />
-      </form>
-    );
+    </form>
+  );
 }
 
 export default Search;
